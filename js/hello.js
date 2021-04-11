f4730282c12f000b98b30878f95aa7130563dad4
aries <2248950919@qq.com>
2021-04-11 17:05:27 +0800

    4-11

---
+++ js/hello.js
@@ -0,0 +1,12 @@
+// const util = {
+//     sum(a, b) {
+//         return a + b;
+//     }
+// }
+
+// export { util }
+export const util = {
+    sum(a, b) {
+        return a + b;
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,5 @@
+import util from "./hello.js"
+import { name, add } from './user.js'
+
+add(2, 3);
+console.log(name);
